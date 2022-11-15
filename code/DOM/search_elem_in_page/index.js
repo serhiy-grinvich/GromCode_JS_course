7e4b82341e9446965a09388cfa70d5b449a24923
serhiy-grinvich <113624514+serhiy-grinvich@users.noreply.github.com>
2022-11-15 15:31:59 +0200

    add solution

---
+++ code/DOM/search_elem_in_page/index.js
@@ -0,0 +1,10 @@
+export function getTitleElement() {
+  const titleEl = document.querySelector('.title');
+  console.dir(titleEl);
+  return titleEl;
+}
+export function getInputElement() {
+  const inputEl = document.querySelector('input[type = text]');
+  console.dir(inputEl);
+  return inputEl;
+}
