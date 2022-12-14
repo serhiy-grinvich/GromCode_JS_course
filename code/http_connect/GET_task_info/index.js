53a244397ca2a657a3106588c2c038497bad2ec1
serhiy-grinvich <113624514+serhiy-grinvich@users.noreply.github.com>
2022-12-14 23:34:09 +0200

    1.1

--- code/http_connect/GET_task_info/index.js
+++ code/http_connect/GET_task_info/index.js
@@ -1,12 +1,12 @@
 const baseUrl = 'https://5e5cf5eb97d2ea0014796f01.mockapi.io/api/v1/tasks';
 
-function getTasksList() {
+export function getTasksList() {
   return fetch(baseUrl).then(response => response.json());
 
   // put your code here
 }
 
-function getTaskById(taskId) {
+export function getTaskById(taskId) {
   //   return fetch(baseUrl).then(response => console.log(response.body));
 
   return getTasksList().then(tasks => tasks.find(task => task.id === taskId));
