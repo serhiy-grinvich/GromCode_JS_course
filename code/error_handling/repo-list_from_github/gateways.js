e72eb9d8f69526f6be3ba41767decc476c0c7c76
serhiy-grinvich <113624514+serhiy-grinvich@users.noreply.github.com>
2022-12-20 21:10:26 +0200

    fix

--- code/error_handling/repo-list_from_github/gateways.js
+++ code/error_handling/repo-list_from_github/gateways.js
@@ -1,17 +1,15 @@
 export const fetchUserData = userName =>
   fetch(`https://api.github.com/users/${userName}`).then(response => {
     if (response.ok) {
-      response.json();
-    } else {
-      throw new Error('Failed to load data');
+      return response.json();
     }
+    throw new Error('Failed to load data');
   });
 
 export const fetchUserRepos = url =>
   fetch(url).then(response => {
     if (response.ok) {
-      response.json();
-    } else {
-      throw new Error('Failed to load data');
+      return response.json();
     }
+    throw new Error('Failed to load data');
   });
